12e60f24c99721a4e295af6a954fa9dac23656d0
Satyabrat Sahoo <satyabrat.me@gmail.com>
2017-08-28 09:29:46 +0000

    [DNM]: msm8916-common: Oreo bringup

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,6 +2,7 @@ BOARD_DTBTOOL_ARGS := -2
 
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
@@ -10,6 +11,8 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 ENABLE_CPUSETS := true
 
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 ifneq ($(FORCE_32_BIT),true)
 TARGET_USES_UNCOMPRESSED_KERNEL := true
 endif
